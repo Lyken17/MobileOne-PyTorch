b09ab514ea202872f5dfdc081f2b28dd2d113a0d
Ligeng Zhu <Lyken17@users.noreply.github.com>
2022-07-26 04:47:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DESCRIPTION = "MobileOne implemented in PyTorch."
 URL = "https://github.com/federicopozzi33/MobileOne-PyTorch"
 EMAIL = "f.pozzi33@campus.unimib.it"
 AUTHOR = "Federico Pozzi"
-REQUIRES_PYTHON = ">=3.8.0"
+REQUIRES_PYTHON = ">=3.5.0"
 VERSION = "0.1.0"
 REQUIRED = ["torch"]
 
